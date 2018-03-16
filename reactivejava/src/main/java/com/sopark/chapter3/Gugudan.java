a68d302f265dec7339cf3b757a9ec8cbd3c24cb4
sopark <sopark@daou.co.kr>
2018-03-16 16:45:02 +0900

    3장 내용 추가

---
+++ reactivejava/src/main/java/com/sopark/chapter3/Gugudan.java
@@ -0,0 +1,36 @@
+package com.sopark.chapter3;
+
+import io.reactivex.Observable;
+import io.reactivex.functions.Function;
+
+import java.util.Scanner;
+
+public class Gugudan {
+
+    public void reactiveV1(){
+        Scanner in = new Scanner(System.in);
+        System.out.println("Gugudan Input :");
+        int dan = Integer.parseInt(in.nextLine());
+
+        Observable<Integer> source = Observable.range(1,9);
+        source.subscribe(row -> System.out.println(dan + " * " + row + " = " + dan*row ));
+    }
+
+    public void reactiveV2(){
+        Scanner in = new Scanner(System.in);
+        System.out.println("Gugudan Input :");
+        int dan = Integer.parseInt(in.nextLine());
+
+        Function<Integer, Observable<String>> gugudan = num ->
+                Observable.range(1,9).map(row -> num + " * " + row + " = " + num * row);
+        Observable<String> source = Observable.just(dan).flatMap(gugudan);
+
+        source.subscribe(System.out::println);
+
+    }
+
+    public static void main(String[] args) {
+        Gugudan gugudan = new Gugudan();
+        gugudan.reactiveV2();
+    }
+}
