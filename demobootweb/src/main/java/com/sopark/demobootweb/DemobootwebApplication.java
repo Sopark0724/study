338bfe1fbb078e7cd182efffb18e973a544f94ca
sopark <sopark@daou.co.kr>
2019-04-03 10:01:41 +0900

    simple spring boot web + test code

---
+++ demobootweb/src/main/java/com/sopark/demobootweb/DemobootwebApplication.java
@@ -0,0 +1,13 @@
+package com.sopark.demobootweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemobootwebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemobootwebApplication.class, args);
+    }
+
+}
---
+++ demobootweb/src/test/java/com/sopark/demobootweb/SampleControllerTest.java
@@ -0,0 +1,27 @@
+package com.sopark.demobootweb;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest
+public class SampleControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void hello() throws Exception {
+        this.mockMvc.perform(get("/hello"))
+                .andDo(print())
+                .andExpect(MockMvcResultMatchers.content().string("hello"));
+    }
+}
