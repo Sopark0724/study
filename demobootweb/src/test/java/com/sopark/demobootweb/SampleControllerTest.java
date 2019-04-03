94507e4511f460735baedba22715caca9890372d
sopark <sopark@daou.co.kr>
2019-04-03 11:54:35 +0900

    ResourceHandler 등록 - resource 파일을 캐시 할수 있음 - If-Modified-Since: 이후에 변경된 파일이 있을 경우에 resource 를 받음 - 캐시되고 있으면 응답코드를 304 를 보냄

--- demobootweb/src/test/java/com/sopark/demobootweb/SampleControllerTest.java
+++ demobootweb/src/test/java/com/sopark/demobootweb/SampleControllerTest.java
@@ -1,5 +1,6 @@
 package com.sopark.demobootweb;
 
+import org.hamcrest.Matchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,12 +8,15 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureWebMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -27,6 +31,23 @@ public class SampleControllerTest {
         this.mockMvc.perform(get("/hello")
                     .param("name", "sopark"))
                 .andDo(print())
-                .andExpect(MockMvcResultMatchers.content().string("hello sopark"));
+                .andExpect(content().string("hello sopark"));
+    }
+
+    @Test
+    public void helloStatic() throws Exception {
+        this.mockMvc.perform(get("/index.html"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(Matchers.containsString("hello index")));
+    }
+
+    @Test
+    public void helloMobile() throws Exception {
+        this.mockMvc.perform(get("/mobile/index.html"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(Matchers.containsString("hello mobile")))
+                .andExpect(header().exists(HttpHeaders.CACHE_CONTROL));
     }
 }
