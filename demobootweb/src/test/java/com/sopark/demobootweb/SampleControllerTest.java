9e6d96af23491b79dedf318f48fc91d1350b6179
sopark <sopark@daou.co.kr>
2019-04-03 11:10:47 +0900

    1. WebConfig 에서 addFormatters 메소드 제거 후 Formatter 를 Component 로 등록 => bean 으로 등록되면 자동으로 formatter 를 Spring boot 에서 등록해줌
    2. 테스트 코드에서는 실패남
    => @@WebMvcTest 는 슬라이싱 테스트 이기 때문에 @Controller 관련 빈만 등록됨 그래서 @Service , @Component 를 알지 못함
    => 해결하기 위해 @SpringBootTest(@Controller, @Service, @Component 등을 사용할수 있음) , @AutoConfigureMockMvc (MockMvc 를 사용하기 위해)

--- demobootweb/src/test/java/com/sopark/demobootweb/SampleControllerTest.java
+++ demobootweb/src/test/java/com/sopark/demobootweb/SampleControllerTest.java
@@ -3,7 +3,10 @@ package com.sopark.demobootweb;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureWebMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
@@ -12,7 +15,8 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest
+@SpringBootTest
+@AutoConfigureMockMvc
 public class SampleControllerTest {
 
     @Autowired
